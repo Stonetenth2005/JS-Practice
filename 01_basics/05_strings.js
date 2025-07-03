1cae5766e83bf34398bd65b100e64ade18fdf8d1
Tushar Srivastava <srivastavatushar2005@gmail.com>
2025-07-03 18:36:12 +0000

    Strings Done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,3 +2,26 @@ const name = 'Tushar'
 const repoCount = 20;
 
 console.log(`Hello my name is ${name}, my repo count is ${repoCount}`);
+
+// declaring string
+const name1 = new String('Tushar')
+
+let subStr = name1.substring(0,4) // returns 'Tush'
+
+
+/* 
+
+STRING METHODS -
+
+1) str.substring(start, endExclusive) // -ve values not allowed
+2) to use -ve values, we use str.slice(start, end)
+3) str.length()
+4) str.toLowerCase()
+5) str.toUpperCase()
+6) str.charAt(4)
+7) str.indexOf('T')
+8) str.trim() // removes spaces from str
+9) str.replace('target', 'changed_value')
+10) str.includes('Tush') // returns boolean
+11) str.split('basis') // returns an array of strings, divided on the basis of given basis
+*/
